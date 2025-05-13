403e3776af72677c01a5504dd700d2a076b5fe91
Mickel Barros <mickelbarros@yahoo.com>
2025-05-13 08:22:05 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,3 +9,17 @@ app.use('/api', enviar);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
+const puppeteer = require('puppeteer-core');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    executablePath: 'C:\Program Files\Google\Chrome\Application', // ou Edge
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  console.log(await page.title());
+
+  await browser.close();
+})();
